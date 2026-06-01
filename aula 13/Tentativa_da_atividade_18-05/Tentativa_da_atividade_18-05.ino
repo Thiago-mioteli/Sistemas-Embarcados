ddce41a6823c8e2b50333d103135764d94134031
Thiago Mioteli <miotelithiago@gmail.com>
2026-06-01 13:11:15 -0300

    Add files via upload

---
+++ aula 13/Tentativa_da_atividade_18-05/Tentativa_da_atividade_18-05.ino
@@ -0,0 +1,72 @@
+/*
+ * Ultrasonic Simple
+ * Prints the distance read by an ultrasonic sensor in
+ * centimeters. They are supported to four pins ultrasound
+ * sensors (liek HC-SC04) and three pins (like PING)))
+ * and Seeed Studio sensors).
+ *
+ * The circuit:
+ * * Module HR-SC04 (four pins) or PING))) (and other with
+ *   three pins), attached to digital pins as follows:
+ * ---------------------    --------------------
+ * | HC-SC04 | Arduino |    | 3 pins | Arduino |
+ * ---------------------    --------------------
+ * |   Vcc   |   5V    |    |   Vcc  |   5V    |
+ * |   Trig  |   12    | OR |   SIG  |   13    |
+ * |   Echo  |   13    |    |   Gnd  |   GND   |
+ * |   Gnd   |   GND   |    --------------------
+ * ---------------------
+ * Note: You do not obligatorily need to use the pins defined above
+ * 
+ * By default, the distance returned by the read()
+ * method is in centimeters. To get the distance in inches,
+ * pass INC as a parameter.
+ * Example: ultrasonic.read(INC)
+ *
+ * created 3 Apr 2014
+ * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
+ * modified 23 Jan 2017
+ * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
+ * modified 03 Mar 2017
+ * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
+ * modified 11 Jun 2018
+ * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
+ *
+ * This example code is released into the MIT License.
+ */
+
+#include <Ultrasonic.h>
+
+const byte sensor_pin = 7;
+Ultrasonic ultrasonic(4, 5);
+int distance;
+int led = 9;
+int led2 = 10;
+int led3 = 11;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+
+  pinMode(sensor_pin, INPUT);
+}
+
+void loop() {
+  // Pass INC as a parameter to get the distance in inches
+  distance = ultrasonic.read();
+  int sensor = digitalRead(sensor_pin);
+
+  Serial.print("Distance in CM: ");
+
+  Serial.println(distance);
+  if(distance < 20 && sensor == 1){
+    digitalWrite(led, 1);    
+  }
+else{
+  digitalWrite(led, 0);
+}
+}
+ 
